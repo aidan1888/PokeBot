bc999aee6aecf8a44d13b05d420a7b5a28da140e
Aidan1888 <aidangallagher1888@gmail.com>
2022-12-28 00:03:28 -0500

    add find method and tweak probabilities

--- main.py
+++ main.py
@@ -175,6 +175,117 @@ async def catch(ctx):
 
 
 @bot.group(with_app_command=True)
+async def find(ctx, arg):
+    count = 0
+    arg = str(arg)
+    with open("pokemon.txt") as file:
+        for line in file:
+            pokemonList = file.readlines()
+    
+    
+    for pokemon in pokemonList:
+        pokeString = pokemon.lower()
+        arg = arg.lower()
+        if pokeString.startswith(arg):
+            new = pokemon
+            count = 1
+    ##count=1
+    final =""
+    name = ""
+    if count == 0:
+        await ctx.reply("That is not a valid Pokemon")
+
+    while (count == 1):
+        count =1
+        ##new=random.choice(pokemonList)
+        new=new.split()
+        if len(new) == 2:
+            num=new[1]
+        elif len(new) == 3:
+            num=new[2]
+        elif len(new) == 4:
+            num=new[2]
+        num=int(num)
+        num = num * 10
+        shiny= num * 10
+        user = ctx.message.author
+        name = str(user)
+        name = name[:-5]
+        count=0
+        
+        prob=random.randrange(num)
+        shinyProb = random.randrange(int(shiny))
+        
+        if prob == 1 and shinyProb > 1 and new[0] != "nothing":
+            list = ["aidan1888-Pikachu"]
+            file = open("PokeDex.txt", "r")
+            list = file.readlines()
+            file.close()
+            list.append(name + "-" + new[0] + "\n")
+
+
+            my_file = "PokeDex.txt"
+            file = open(my_file, "w")
+            z= 0
+            v = len(list)
+            st=""
+            while z != v:
+                file.write(list[z])
+                z+=1
+            file.close()
+
+            embed=discord.Embed(title=new[0], description= f"{name} has caught " + new[0] + "!")
+            file=discord.File("C:/Users/aidan/Desktop/PokeBot/Pokemon Dataset/" + new[0] + ".png", filename=new[0] + ".png")
+            embed.set_image(url="attachment://" + new[0] + ".png")
+
+            
+            
+            await ctx.reply(file=file, embed=embed)
+
+            count=2
+        elif shinyProb < 2 and prob == 1 and new[0] != "nothing":
+
+            embed=discord.Embed(title=new[0], description= f"{name} has caught SHINY " + new[0] + "!")
+            shinylist = ["\n"]
+            file = open("ShinyDex.txt", "r")
+            shinylist = file.readlines()
+            file.close()
+            shinylist.append(name + "-*" + new[0] + "\n")
+
+
+            myFile = "ShinyDex.txt"
+            file = open(myFile, "w")
+            z= 0
+            v = len(shinylist)
+            st=""
+            while z != v:
+                file.write(shinylist[z])
+                z+=1
+            file.close()
+            file=discord.File("C:/Users/aidan/Desktop/PokeBot/shiny/" + new[0] + ".png", filename=new[0] + ".png")
+            embed.set_image(url="attachment://" + new[0] + ".png")
+            
+            await ctx.reply(file=file, embed=embed)
+            
+            count=2
+
+        elif new[0] == "nothing":
+            embed=discord.Embed(title=new[0], description= f"{name} caught " + new[0] + "!")
+            file=discord.File("C:/Users/aidan/Desktop/PokeBot/Pokemon Dataset/" + new[0] + ".png", filename=new[0] + ".png")
+            embed.set_image(url="attachment://" + new[0] + ".png")
+            await ctx.reply(file=file, embed=embed)
+
+        elif prob != 1:
+            embed=discord.Embed(title=new[0] + " has fled!!", description= f"{name} almost caught " + new[0] + ", but it fled!")
+            file=discord.File("C:/Users/aidan/Desktop/PokeBot/Pokemon Dataset/fled.gif", filename="fled.gif")
+            embed.set_image(url="attachment://fled.gif")
+            await ctx.reply(file=file, embed=embed)
+
+        else:
+            count=1
+
+
+@bot.group(with_app_command=True)
 async def pokedex(ctx):
     user = ctx.message.author
     name = str(user)
@@ -298,5 +409,9 @@ async def shiny(ctx, arg):
         file = discord.File("C:/Users/aidan/Desktop/PokeBot/shinydex.png", filename="shinydex.png")
         embed.set_image(url="attachment://shinydex.png")
         await ctx.reply(file=file, embed=embed)
+
+
+
+
 
 bot.run(TOKEN)
