ad42e8300526a81dfcb8265aed76ba36929ecc6a
Aidan1888 <aidangallagher1888@gmail.com>
2022-12-27 23:13:50 -0500

    fix help command

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_ready():
 async def help(ctx):
     embed=discord.Embed(title="COMMANDS", description= f"1.\t!catch - Catch a Pokemon\n2.\t!pokedex"
     + " - Check your Pokedex and your collection progress\n3.\t!shinydex - Check your shiny Pokedex and your collection progress"
-    + "/n4.\t!have + argument - Check if you have a specific Pokemon by typing the command followed by the name of the Pokemon/n5."
+    + "\n4.\t!have + argument - Check if you have a specific Pokemon by typing the command followed by the name of the Pokemon\n5."
     + "\t!shiny + argument - Check if you have a specific shiny Pokemon by typing the command followed by the name of the Pokemon")
     await ctx.reply(embed=embed)
 
